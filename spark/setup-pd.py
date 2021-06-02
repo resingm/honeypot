a1cfe4dd20713af9ac19dd86a40e2edcb523eda5
Max Resing <max.resing@protonmail.com>
2021-06-02 20:40:38 +0200

    Updated setup-pd; removed configuration.py ; replaced by configuration.dist.py

--- spark/setup-pd.py
+++ spark/setup-pd.py
@@ -1,19 +1,10 @@
+# The setup-pd.py assumes that the data is available offline
+# This should ensure a quick import into Jupyter.
+
 import pandas as pd
 import numpy as np
-import psycopg2
 
-dbc = psycopg2.connect(
-    database=configuration['pg_database'],
-    host=configuration['pg_host'],
-    port=configuration['pg_port'],
-    user=configuration['pg_user'], 
-    password=configuration['pg_password'],
-)
 
-df_log = pd.read_sql_query(
-    f"SELECT * FROM log where '{dates[0]}' <= timestamp AND timestamp < '{dates[-1]}'",
-    con=dbc,
-    index_col=['id'],
-)
-
-df.head()
+df_log = pd.read_csv(csv_log)
+df_dataplane = pd.read_csv(csv_dataplane)
+df_ip = pd.read_csv(csv_ip)
