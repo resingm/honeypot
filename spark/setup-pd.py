bbddc8fee4ec2cea2f1266f46149b3c69ae29a9b
Max Resing <max.resing@protonmail.com>
2021-06-10 21:44:10 +0200

    Bug fix in setup-pd.py

--- spark/setup-pd.py
+++ spark/setup-pd.py
@@ -3,8 +3,10 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import geopandas as gpd
 
-
-df_log = pd.read_csv(csv_log)
-df_dataplane = pd.read_csv(csv_dataplane)
+df_log = pd.read_csv(csv_log, parse_dates=['sync_ts', 'timestamp'])
+df_log = df_log[df_log.origin_id != 2]
+df_dataplane = pd.read_csv(csv_dataplane, parse_dates=['sync_ts', 'timestamp'])
 df_ip = pd.read_csv(csv_ip)
